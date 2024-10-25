5fbf03384a2185626df5621bc84e28ef8f34a945
d9beuD <d9beuD@users.noreply.github.com>
2024-10-25 15:32:38 +0200

    Add Link entity and LinkRepository

---
+++ src/Entity/Link.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LinkRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: LinkRepository::class)]
+class Link
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\Url]
+    #[Assert\NotBlank]
+    #[ORM\Column(length: 500)]
+    private ?string $destinationUrl = null;
+
+    #[ORM\Column(options: ['default' => true])]
+    private ?bool $enabled = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDestinationUrl(): ?string
+    {
+        return $this->destinationUrl;
+    }
+
+    public function setDestinationUrl(string $destinationUrl): static
+    {
+        $this->destinationUrl = $destinationUrl;
+
+        return $this;
+    }
+
+    public function isEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): static
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+}
